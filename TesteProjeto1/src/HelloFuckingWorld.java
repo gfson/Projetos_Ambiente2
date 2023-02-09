aa36e9e9fcd3e4a93303266a8207a40e51b872a6
informatica02 <informatica02@BRJDCMAINF02.grupocma.local>
2023-02-09 16:48:10 -0300

    Commit inicial

---
+++ TesteProjeto1/src/HelloFuckingWorld.java
@@ -0,0 +1,12 @@
+
+public class HelloFuckingWorld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("Ana, banana...");
+		System.out.println("É um real a palma da banana");
+		System.out.println("");
+	}
+
+}
