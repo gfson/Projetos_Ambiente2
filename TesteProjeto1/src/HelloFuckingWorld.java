9b6f0e83e8c6bda90ccdcba0fed150924db3b8ce
gilson <silvagilsonf@gmail.com>
2023-03-15 16:44:57 -0300

    Teste de envio 15/03/23

--- TesteProjeto1/src/HelloFuckingWorld.java
+++ TesteProjeto1/src/HelloFuckingWorld.java
@@ -1,12 +1,29 @@
+import javax.management.timer.Timer;
 
 public class HelloFuckingWorld {
+	int delay = 0;
+	int interval = 1000;
+	Timer timer = new Timer();
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		
+/*
+		timer.scheduleAtFixedRate (new TimerTask) {}
 		System.out.println("Ana, banana...");
-		System.out.println("É um real a palma da banana");
-		System.out.println("");
+		System.out.println("É um real a palma da banana\n");
+		
+*/
+		public void TarefaAnaBanana() {
+			int delay = 0;   // delay de 0 seg.
+			int interval = 2000;  // intervalo de 2 seg.        
+	
+			timer2.scheduleAtFixedRate(new TimerTask() {
+					public void run() {
+						System.out.println("Olá World no console");
+					}
+				}, delay, interval);
+		}
+		
 	}
-
 }
